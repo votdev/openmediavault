3431dc0826e3ccac30242d24e9a21a34a2153a63
Volker Theile <votdev@gmx.de>
2025-08-10 12:30:59 +0200

    openmediavault 8 / Debian 13
    Signed-off-by: Volker Theile <votdev@gmx.de>

--- vagrant/debian/install.sh
+++ vagrant/debian/install.sh
@@ -44,33 +44,5 @@ apt-get install --yes gnupg
 wget --quiet --output-document=- https://packages.openmediavault.org/public/archive.key | \
 	gpg --dearmor --yes --output "/usr/share/keyrings/openmediavault-archive-keyring.gpg"
 
-# Install openmediavault.
-cat <<EOF >> /etc/apt/sources.list.d/openmediavault.list
-deb [signed-by=/usr/share/keyrings/openmediavault-archive-keyring.gpg] http://packages.openmediavault.org/public sandworm main
-# deb [signed-by=/usr/share/keyrings/openmediavault-archive-keyring.gpg] http://downloads.sourceforge.net/project/openmediavault/packages sandworm main
-## Uncomment the following line to add software from the proposed repository.
-# deb [signed-by=/usr/share/keyrings/openmediavault-archive-keyring.gpg] http://packages.openmediavault.org/public sandworm-proposed main
-# deb [signed-by=/usr/share/keyrings/openmediavault-archive-keyring.gpg] http://downloads.sourceforge.net/project/openmediavault/packages sandworm-proposed main
-## This software is not part of OpenMediaVault, but is offered by third-party
-## developers as a service to OpenMediaVault users.
-# deb [signed-by=/usr/share/keyrings/openmediavault-archive-keyring.gpg] http://packages.openmediavault.org/public sandworm partner
-# deb [signed-by=/usr/share/keyrings/openmediavault-archive-keyring.gpg] http://downloads.sourceforge.net/project/openmediavault/packages sandworm partner
-EOF
-apt-get update
-apt-get --yes --auto-remove --show-upgraded \
-	--allow-downgrades --allow-change-held-packages \
-	--no-install-recommends \
-	--option DPkg::Options::="--force-confdef" \
-	--option DPkg::Options::="--force-confold" \
-	install openmediavault
-
-# Populate the database.
-omv-confdbadm populate
-
-# Deploy the /etc/hosts file to ensure the hostname can be resolved
-# properly for IPv4 and IPv6. Otherwise building the Salt grains
-# (core.fqdns and core.ip_fqdn) will take a very long time.
-omv-salt deploy run hosts
-
 # Display the login information.
 cat /etc/issue
